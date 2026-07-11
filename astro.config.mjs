f766e8ce6354345bd18e283ef34bda38f143fe34
Abdullah Khokhar <abdullah.vocab@gmail.com>
2026-07-10 20:28:21 -0400

    basename update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // If you are deploying to a GitHub Pages subpath (e.g. username.github.io/reponame),
   // uncomment the next line and replace 'reponame' with your repository name.
-  // base: '/reponame',
+  base: '/excitestem',
 });
