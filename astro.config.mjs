33d7c440a0db2b05f21c9adbdcd18adc2154c2e2
Abdullah Khokhar <abdullah.vocab@gmail.com>
2026-07-13 18:47:49 -0400

    try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ export default defineConfig({
   // The site domain (adjust if using a custom domain)
   site: 'https://abdullah-makhokhar.github.io',
   // The base path for your repository
-  base: '/',
+  base: '/excitestem',
 });
